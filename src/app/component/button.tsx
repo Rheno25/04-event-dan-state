9c343470cab067031563246bf5f0f4fe41b7f780
Rheno Rayhan <rayhanrheno@gmail.com>
2024-04-04 20:47:58 +0700

    Jawaban Praktikum 3

--- src/app/component/button.tsx
+++ src/app/component/button.tsx
@@ -1,9 +1,23 @@
 export function Button_2({messageContent, buttonName}){
     return(
         <button 
-            className="bg-blue-500 hover : bg-blue-700 text-white p-2 rounded"
+            className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
             onClick={() => alert(messageContent)}>
             {buttonName}
+        </button>
+    );
+}
+
+export function Button_3({messageContent, buttonName}){
+    return(
+        <button
+        className="bg-green-400 hover:bg-green-700 text-white p-2 rounded m-2"
+        onClick={(e) => {
+                e.stopPropagation();
+                alert(messageContent)
+            }
+        }>
+        {buttonName}
         </button>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Button_1, {Button_2} from "./component/button";
+import Button_1, {Button_2, Button_3} from "./component/button";
 
 export default function Home() {
   return (
@@ -9,6 +9,11 @@ export default function Home() {
       <Button_1 />
       <hr></hr>
       <Button_2 messageContent="This is my message" buttonName="Message" />
+    </div>
+    <br></br>
+    <div className="bg-red-300" onClick={() => alert('Parent Element: Div')}>
+      <Button_3 messageContent="Child Element : Button-1" buttonName="Button-1" />
+      <Button_3 messageContent="Child Element : Button-2" buttonName="Button-2" />
     </div>
     </>
   );
